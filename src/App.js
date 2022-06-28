7e7ad826f425f41e9effa4df4568ba2b3b0d5974
ankitpanchal534 <ankitpanchal534@gmail.com>
2022-06-28 10:23:52 +0530

    food-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Store/store";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Cart from "./Components/Cart";
+import Menu from "./Components/Menu";
+import Checkout from "./Components/Checkout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="menu" element={<Menu />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
